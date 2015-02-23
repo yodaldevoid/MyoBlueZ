b15d576ab0a1026a94578978bd4fb65c8caf6cc4
Gabriel Smith <ga29smith@gmail.com>
2015-02-22 21:48:24 -0500

    Fixed errors and warning found while compiling

--- myo-bluez.c
+++ myo-bluez.c
@@ -2,12 +2,13 @@
 #include <glib.h>
 #include <gio/gio.h>
 #include <string.h>
+#include <errno.h>
 
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/hci.h>
 #include <bluetooth/hci_lib.h>
 
-static const PAYLOAD_END[] = {0x06, 0x42, 0x48, 0x12, 0x4A, 0x7F,
+static const unsigned char PAYLOAD_END[] = {0x06, 0x42, 0x48, 0x12, 0x4A, 0x7F,
                                 0x2C, 0x48, 0x47, 0xB9, 0xDE, 0x04,
                                 0xA9, 0x01, 0x00, 0x06, 0xD5};
 
@@ -16,8 +17,34 @@ static GMainLoop *loop;
 
 static int new;
 
+static GDBusObjectManagerClient *_manager;
 static GDBusProxy *adapter;
 static GDBusProxy *device;
+
+void myo_connect();
+void myo_initialize();
+
+void disconnect_cb(GDBusProxy *proxy, GVariant *changed, GStrv invalid,
+                    gpointer user_data) {
+    GVariantIter *iter;
+    const gchar *key;
+    GVariant *value;
+    
+    //check if peroperty was "connected"
+    if(g_variant_n_children(changed) > 0) {
+        g_variant_get(changed, "a{sv}", &iter);
+        while(g_variant_iter_loop(iter, "{&sv}", &key, &value)) {
+            if(strcmp(key, "Connected") == 0) {
+                if(!g_variant_get_boolean(value)) {
+                    //disconnected
+                    //TODO: check for success
+                    myo_connect();
+                }
+            }
+        }
+        g_variant_iter_free (iter);
+    }
+}
 
 void interface_added_cb(GDBusObjectManager *manager, GDBusObject *object, GDBusInterface *interface,
                     gpointer user_data) {
@@ -25,7 +52,7 @@ void interface_added_cb(GDBusObjectManager *manager, GDBusObject *object, GDBusI
     bdaddr_t *addr;
     GDBusProxy *proxy;
     GVariant *addrVar, *reply;
-    char *addrStr;
+    const gchar *addrStr;
     bdaddr_t cmp;
     
     info = g_dbus_interface_get_info(interface);
@@ -48,7 +75,7 @@ void interface_added_cb(GDBusObjectManager *manager, GDBusObject *object, GDBusI
                 //TODO: check for error
                 
                 //set watcher for disconnect
-                g_signal_connect(device, "g-properties-changed", disconnect_cb, NULL);
+                g_signal_connect(device, "g-properties-changed", G_CALLBACK(disconnect_cb), NULL);
                 
                 myo_connect();
             }
@@ -58,7 +85,7 @@ void interface_added_cb(GDBusObjectManager *manager, GDBusObject *object, GDBusI
     }
 }
 
-void myo_scan(int adapter_id, bdaddr_t *addr) {
+int myo_scan(int adapter_id, bdaddr_t *addr) {
     int dd, err, len;
     
     unsigned char buf[HCI_MAX_EVENT_SIZE];
@@ -72,25 +99,23 @@ void myo_scan(int adapter_id, bdaddr_t *addr) {
     char adapterPath[16];
     GVariant *reply;
     
-    addr = NULL;
-    
     dd = hci_open_dev(adapter_id);
     if (dd < 0) {
 		perror("Could not open device");
-		return;
+		return -1;
 	}
     
     err = hci_le_set_scan_parameters(dd, 1, htobs(0x0010), htobs(0x0010),
 						LE_PUBLIC_ADDRESS, 0, 10000);
     if (err < 0) {
 		perror("Set scan parameters failed");
-		return;
+		return -1;
 	}
     
     err = hci_le_set_scan_enable(dd, 1, 1, 10000);
     if (err < 0) {
 		perror("Enable scan failed");
-		return;
+		return -1;
 	}
     
     printf("Scanning...\n");
@@ -101,7 +126,7 @@ void myo_scan(int adapter_id, bdaddr_t *addr) {
 		printf("Could not get socket options\n");
         
         //TODO: turn off scanning
-		return;
+		return -1;
 	}
     
     //make new filter
@@ -114,7 +139,7 @@ void myo_scan(int adapter_id, bdaddr_t *addr) {
 		printf("Could not set socket options\n");
         
         //TODO: turn off scanning
-		return;
+		return -1;
 	}
     
     done = FALSE;
@@ -174,33 +199,13 @@ void myo_scan(int adapter_id, bdaddr_t *addr) {
             "org.bluez", adapterPath, "org.bluez.Adapter1", NULL, &error);
     //TODO: check for error
     
-    g_signal_connect(manager, "interface-added", interface_added_cb, addr);
+    g_signal_connect(_manager, "interface-added", G_CALLBACK(interface_added_cb), addr);
     
     reply = g_dbus_proxy_call_sync(adapter, "StartDiscovery", NULL, G_DBUS_CALL_FLAGS_NONE, -1, NULL, &error);
     //TODO: check for error
     g_variant_unref(reply);
-}
-
-void disconnect_cb(GDBusProxy *proxy, GVariant *changed, GStrv invalid,
-                    gpointer user_data) {
-    GVariantIter *iter;
-    const gchar *key;
-    GVariant *value;
     
-    //check if peroperty was "connected"
-    if(g_variant_n_children(changed) > 0) {
-        g_variant_get(changed_properties, "a{sv}", &iter);
-        while(g_variant_iter_loop(iter, "{&sv}", &key, &value)) {
-            if(strcmp(key, "Connected") == 0) {
-                if(!g_variant_get_boolean(value)) {
-                    //disconnected
-                    //TODO: check for success
-                    myo_connect();
-                }
-            }
-        }
-        g_variant_iter_free (iter);
-    }
+    return 0;
 }
 
 void myo_connect() {
@@ -216,35 +221,25 @@ void myo_connect() {
     myo_initialize();
 }
 
-static void char_read_cb(guint8 status, const guint8 *pdu, guint16 plen, gpointer user_data) {
-	if (status != 0) {
-		perror("Characteristic value/descriptor read failed: %s\n", att_ecode2str(status));
-		return;
-	}
-
-	readValueSize = dec_read_resp(pdu, plen, readValue, sizeof(readValue));
-	if (readValueSize < 0) {
-		perror("Protocol error\n");
-	}
-}
-
-void read(int handle) {
+void read_char(int handle) {
     
 }
 
-void write(int handle, unsigned char *data, int dataLen) {
+void write_char(int handle, unsigned char *data, int dataLen) {
     
 }
 
 void myo_initialize() {
     unsigned char writeValue[16];
     
+    unsigned short C;
+    unsigned char emg_hz, emg_smooth, imu_hz;
+    
     //read firmware version
-    read(0x17)
+    read_char(0x17);
     printf("firmware version: %d.%d.%d.%d\n", readValue[4], readValue[5], readValue[6], readValue[7]);
     
     new = readValue[4];
-    freeReadArray();
     //if old do the thing
     //else do the other thing (configure)
     if(!new) {
@@ -253,54 +248,53 @@ void myo_initialize() {
         writeValue[1] = 0x02;
         writeValue[2] = 0x00;
         writeValue[3] = 0x00;
-        write(0x19, writeValue, 4);
+        write_char(0x19, writeValue, 4);
         
         writeValue[1] = 0x00;
-        write(0x2f, writeValue, 2);
-        write(0x2c, writeValue, 2);
-        write(0x32, writeValue, 2);
-        write(0x35, writeValue, 2);
+        write_char(0x2f, writeValue, 2);
+        write_char(0x2c, writeValue, 2);
+        write_char(0x32, writeValue, 2);
+        write_char(0x35, writeValue, 2);
 
         //enable EMG data
-        write(0x28, writeValue, 2);
+        write_char(0x28, writeValue, 2);
         //enable IMU data
-        write(0x1d, writeValue, 2);
+        write_char(0x1d, writeValue, 2);
 
         //Sampling rate of the underlying EMG sensor, capped to 1000. If it's less than 1000, emg_hz is correct. If it is greater, the actual framerate starts dropping inversely. Also, if this is much less than 1000, EMG data becomes slower to respond to changes. In conclusion, 1000 is probably a good value.
-        C = 1000
-        emg_hz = 50
+        C = 1000;
+        emg_hz = 50;
         //strength of low-pass filtering of EMG data
-        emg_smooth = 100
+        emg_smooth = 100;
 
-        imu_hz = 50
+        imu_hz = 50;
 
         //send sensor parameters, or we don't get any data
         writeValue[0] = 2;
         writeValue[1] = 9;
         writeValue[2] = 2;
         writeValue[3] = 1;
-        *((short*) &(writeValue + 4)) = C;
+        *((short*) (writeValue + 4)) = C;
         writeValue[6] = emg_smooth;
         writeValue[7] = C/emg_hz;
         writeValue[8] = imu_hz;
         writeValue[9] = 0;
         writeValue[10] = 0;
-        write(0x19, writeValue, 11);
+        write_char(0x19, writeValue, 11);
     } else {
-        read(0x03);
+        read_char(0x03);
         printf("device name: %s", readValue);
-        freeReadValue();
 
         //enable IMU data
         writeValue[0] = 0x01;
         writeValue[1] = 0x00;
-        write(0x1d, writeValue, 2)
+        write_char(0x1d, writeValue, 2);
         //enable on/off arm notifications
         writeValue[0] = 0x02;
-        write(0x24, writeValue, 2)
+        write_char(0x24, writeValue, 2);
 
         //self.write_attr(0x19, b'\x01\x03\x00\x01\x01')
-        start_raw()
+        //start_raw();
     }
 }
 
@@ -308,26 +302,22 @@ int main(void) {
     bdaddr_t dst;
     int adapter_id;
     
-    GDBusObjectManagerClient *manager;
-    GVariant *reply;
-    
-    error = g_error_new();
+    error = NULL;
     loop = g_main_loop_new(NULL, FALSE);
     
-    manager = (GDBusObjectManagerClient*) g_dbus_object_manager_client_new_for_bus_sync(G_BUS_TYPE_SYSTEM,
-            G_DBUS_OBJECT_MANAGER_CLIENT_FLAGS_NONE, "org.bluez", "/", NULL, NULL, NULL, &error);
+    _manager = (GDBusObjectManagerClient*) g_dbus_object_manager_client_new_for_bus_sync(G_BUS_TYPE_SYSTEM,
+            G_DBUS_OBJECT_MANAGER_CLIENT_FLAGS_NONE, "org.bluez", "/", NULL, NULL, NULL, NULL, &error);
     //TODO: check for error
     
     adapter_id = hci_get_route(NULL);
     
     //scan for Myo
-    scan_myo(adapter_id, &dst);
-    if(NULL == dst) {
+    if(!myo_scan(adapter_id, &dst)) {
         perror("Error finding Myo!");
         if(NULL != adapter) {
             g_object_unref(adapter);
         }
-        g_object_unref(manager);
+        g_object_unref(_manager);
         return 1;
     }
     
